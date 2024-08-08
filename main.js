fdeeccdb3eaccec6883e1eb7a0ba62eabcb4a3c7
Akshay Nair <akshayakn6@gmail.com>
2024-08-08 00:21:38 -0400

    Modal complete

---
+++ main.js
@@ -0,0 +1,26 @@
+document.querySelectorAll(".portfolio-item").forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("open");
+        
+        document.querySelector(`.modal#${item.dataset.modalId}`).classList.toggle("open");
+        document.querySelectorAll(`.modal#${item.dataset.modalId} *`).forEach((item,i) => {
+            item.classList.add("fade-up");
+            item.style.animationDuration = `${i}s`;
+        });
+        document.body.style.overflow = "hidden";
+    });
+});
+document.querySelectorAll(".close").forEach(item => {
+    item.addEventListener("click", () => {
+        document.querySelectorAll(".modal").forEach(item => {
+            item.classList.remove("open");
+        });
+        document.querySelectorAll(".portfolio-item").forEach(item => {
+            item.classList.remove("open");
+        });
+        document.querySelectorAll(`.modal *`).forEach(item => {
+            item.classList.remove("fade-up");
+        });
+        document.body.style.overflow = "visible";
+    });
+});
